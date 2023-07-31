531d234bd8a4c93e0f4b27efc1c4b373f6bccf07
GlebSats <133129364+GlebSats@users.noreply.github.com>
2023-07-31 12:57:01 +0200

    Update WebSocketConnection.h

--- WebSocketConnection.h
+++ WebSocketConnection.h
@@ -1,1 +1,21 @@
-
+class WebSocketConnection : public StartServer {
+public:
+	WebSocketConnection(const char* listeningPort, LPCWSTR serverIP, INTERNET_PORT serverPort);
+	~WebSocketConnection();
+	void serverInitialization() override;
+	void serverHandler() override;
+private:
+	void WaitResponseFromServer();
+	void connectToServer();
+	void stopServer();
+private:
+	bool SendResponseStatus;
+	bool ResponseStatus;
+	HANDLE serverSendResponse;
+	HINTERNET SessionHandle;
+	HINTERNET ConnectionHandle;
+	HINTERNET RequestHandle;
+	HINTERNET WebSocketHandle;
+	LPCWSTR serverIP;
+	INTERNET_PORT serverPort;
+};
