2148a49186908f2011686044de5fe99aa9343289
GlebSats <133129364+GlebSats@users.noreply.github.com>
2023-08-04 10:55:07 +0200

    Create ProxyServerlib.h

---
+++ ProxyServerlib.h
@@ -0,0 +1,96 @@
+class ProxyServer {
+public:
+	ProxyServer();
+	virtual ~ProxyServer() = 0;
+	virtual void serverInitialization() = 0;
+	virtual void WaitingForClients() = 0;
+	virtual void connectToTargetServer() = 0;
+public:
+	HANDLE* stopEvent;
+};
+
+class TCPServer: public ProxyServer
+{
+public:
+	TCPServer(const char* listeningPort); // Constructor
+	~TCPServer();
+	void serverInitialization() override;
+	void WaitingForClients() override;
+private:
+	//Prohibited methods
+	void connectToTargetServer() override {}
+	//
+	void initSockets(); // Function initiates use of the Winsock DLL
+	void bindSocket(); // Function associates a local address with a socket
+	void listenState(); // Set socket to listen state
+	void stopServer(); // Freeing memory
+	void createSockInfo(const char* ip, const char* port, addrinfo** sockInfo); // Create addrinfo and translate host name to address
+	void createNewSocket(SOCKET& new_socket, addrinfo* sockInfo); // Create socket with addrinfo parameters
+	void acceptConnection();
+	TCPServer(const TCPServer&) = delete; // Copy not allowed
+	void operator=(const TCPServer&) = delete; // Assignment not allowed
+private:
+	const char* listeningPort;
+	addrinfo* lisSockInfo;
+	SOCKET lis_socket;
+	WSAEVENT clientConnectionRequest;
+	int errState;
+	WSADATA wsData;
+	SOCKET client_socket;
+};
+
+class TCPClient : public ProxyServer {
+public:
+	TCPClient(const char* serverIP, const char* serverPort);
+	~TCPClient();
+	void connectToTargetServer() override;
+	//void serverHandler() override;
+private:
+	//Prohibited methods
+	virtual void serverInitialization() override {}
+	virtual void WaitingForClients() override {}
+	//
+	void createSockInfo(const char* ip, const char* port, addrinfo** sockInfo); // Create addrinfo and translate host name to address
+	void createNewSocket(SOCKET& new_socket, addrinfo* sockInfo); // Create socket with addrinfo parameters
+	//void sockCommunication();
+	//void createSocketEvents();
+	//void closeConnection();
+	TCPClient(const TCPClient&) = delete; // Copy not allowed
+	void operator=(const TCPClient&) = delete; // Assignment not allowed
+private:
+	const char* serverIP;
+	const char* serverPort;
+	addrinfo* serverSockInfo;
+	SOCKET server_socket;
+	WSAEVENT bufToClientHasData;
+	WSAEVENT bufToServHasData;
+	WSAEVENT clientReadySend;
+	WSAEVENT serverReadySend;
+	int errState;
+};
+
+class WebSocketConnection : public StartServer {
+public:
+	WebSocketConnection(const char* listeningPort, LPCWSTR serverIP, INTERNET_PORT serverPort);
+	~WebSocketConnection();
+	void serverInitialization() override;
+	void serverHandler() override;
+private:
+	void WaitResponseFromServer();
+	void connectToServer();
+	void sockCommunication();
+	void createSocketEvents();
+	void stopServer();
+private:
+	bool SendResponseStatus;
+	bool ResponseStatus;
+	HANDLE serverSendResponse;
+	HINTERNET SessionHandle;
+	HINTERNET ConnectionHandle;
+	HINTERNET RequestHandle;
+	HINTERNET WebSocketHandle;
+	LPCWSTR serverIP;
+	INTERNET_PORT serverPort;
+};
+
+void proxyConnection(ProxyServer& server, ProxyServer& client, HANDLE* stopEvent);
