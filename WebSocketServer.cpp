5e016abc81bcca2efd2942da0e7d6ea10fd63a15
GlebSats <133129364+GlebSats@users.noreply.github.com>
2023-08-16 07:30:14 +0200

    Update and rename WebSocket to WebSocketServer.cpp

--- WebSocketServer.cpp
+++ WebSocketServer.cpp
@@ -1,28 +1,5 @@
-//h
-class WebSocketServer : public ProxyConnection {
-public:
-	WebSocketServer(LPCWSTR serverIP, INTERNET_PORT serverPort);
-	~WebSocketServer();
-	void Initialization() override;
-	void Connection() override;
-	void Handler() override;
-	int sendData(const char* pData, int length) override;
-	void receiveData() override;
-	void closeConnection() override;
-private:
-	void WaitResponseFromServer();
-private:
-	bool SendResponseStatus;
-	bool ReceiveResponseStatus;
-	HANDLE serverSendResponse;
-	HINTERNET SessionHandle;
-	HINTERNET ConnectionHandle;
-	HINTERNET RequestHandle;
-	HINTERNET WebSocketHandle;
-	LPCWSTR serverIP;
-	INTERNET_PORT serverPort;
-};
-//cpp
+#include "WebSocketServer.h"
+
 WebSocketServer::WebSocketServer(LPCWSTR serverIP, INTERNET_PORT serverPort):
 	serverIP(serverIP),
 	serverPort(serverPort),
