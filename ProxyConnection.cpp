d96b8b8f7979b2954fe955740fc29769fe735bcd
GlebSats <133129364+GlebSats@users.noreply.github.com>
2023-09-04 13:18:50 +0200

    Update ProxyConnection.cpp

--- ProxyConnection.cpp
+++ ProxyConnection.cpp
@@ -4,16 +4,47 @@ ProxyConnection::ProxyConnection() :
 	stopEvent(nullptr),
 	disconnect(NULL),
 	readySend(NULL),
+	readyReceive(NULL),
 	dataToSend(NULL),
 	dataInReceiveBuffer(0),
-	indexForRecData(0),
-	readyRecv(false)
+	indexForRecData(0)
 {
 	ZeroMemory(&receiveBuffer, sizeof(receiveBuffer));
 }
 
 ProxyConnection::~ProxyConnection()
 {
+}
+
+void ProxyConnection::subtractData(const int send_data)
+{
+	dataInReceiveBuffer -= send_data;
+	indexForRecData += send_data;
+
+	if (dataInReceiveBuffer != 0) {
+		SetEvent(dataToSend);
+	}
+	else {
+		ResetEvent(dataToSend);
+	}
+}
+
+int ProxyConnection::WaitingToSend()
+{
+	HANDLE eventArr[3] = { *stopEvent, disconnect, readyReceive };
+	int eventResult = WaitForMultipleObjects(3, eventArr, FALSE, INFINITE);
+
+	if (eventResult == WAIT_FAILED) {
+		writeLog("Error while waiting for events: ", GetLastError());
+		SetEvent(disconnect);
+		return -1;
+	}
+
+	if ((eventResult == WAIT_OBJECT_0) || (eventResult == WAIT_OBJECT_0 + 1)) {
+		return -1;
+	}
+
+	return 0;
 }
 
 void ProxyConnection::eventsCreation()
@@ -25,6 +56,11 @@ void ProxyConnection::eventsCreation()
 
 	readySend = CreateEvent(NULL, TRUE, FALSE, NULL);
 	if (readySend == NULL) {
+		throw ServException("Create Event failed: ", GetLastError());
+	}
+
+	readyReceive = CreateEvent(NULL, TRUE, FALSE, NULL);
+	if (readyReceive == NULL) {
 		throw ServException("Create Event failed: ", GetLastError());
 	}
 
@@ -42,6 +78,10 @@ void ProxyConnection::eventsDeleting()
 	if (readySend != NULL) {
 		CloseHandle(readySend);
 		readySend = NULL;
+	}
+	if (readyReceive != NULL) {
+		CloseHandle(readyReceive);
+		readyReceive = NULL;
 	}
 	if (disconnect != NULL) {
 		CloseHandle(disconnect);
