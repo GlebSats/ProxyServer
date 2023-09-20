f313ffb7287a741086116accfa4f5448e03b7a51
GlebSats <133129364+GlebSats@users.noreply.github.com>
2023-09-20 22:10:17 +0200

    Create writeLogHex.h

---
+++ writeLogHex.h
@@ -0,0 +1,10 @@
+#ifndef WLH_H
+#define WLH_H
+#include <string>
+#include <fstream>
+#include <ctime>
+#include <Windows.h>
+using namespace std;
+
+void writeLogHex(std::string message, char first, char second, char third);
+#endif
